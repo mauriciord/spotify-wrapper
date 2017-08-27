c18710b483c2309d4bf4f6b0c7be178a3ad75a08
Maurício R Duarte <mauriciord@me.com>
2017-08-27 20:39:08 -0300

    Tests ok

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -2,17 +2,27 @@ import chai, { expect } from 'chai';
 import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
 import sinonStubPromise from 'sinon-stub-promise';
+import { search, searchAlbums, searchArtists, searchPlaylists, searchTracks } from '../src/main';
+
 chai.use(sinonChai);
 sinonStubPromise(sinon);
 
 global.fetch = require('node-fetch');
 
-import { search, searchAlbums, searchArtists, searchPlaylists, searchTracks } from '../src/main';
-
 describe('Spotify Wrapper', () => {
+  let fetchedStub;
+  let promise;
+
+  beforeEach(() => {
+    fetchedStub = sinon.stub(global, 'fetch');
+    promise = fetchedStub.returnsPromise();
+  });
+
+  afterEach(() => {
+    fetchedStub.restore();
+  });
 
   describe('smoke tests', () => {
-
     it('should exist the search method', () => {
       expect(search).to.exist;
     });
@@ -32,21 +42,9 @@ describe('Spotify Wrapper', () => {
     it('should exist the searchPlaylists method', () => {
       expect(searchPlaylists).to.exist;
     });
-
   });
 
   describe('Generic Search', () => {
-    let fetchedStub;
-    let promise;
-
-    beforeEach(() => {
-      fetchedStub = sinon.stub(global, 'fetch');
-      fetchedStub.returnsPromise();
-    });
-
-    afterEach(() => {
-      fetchedStub.restore();
-    });
 
     it('should call fetch function', () => {
       const artists = search();
@@ -78,6 +76,57 @@ describe('Spotify Wrapper', () => {
       const artists = search('Audioslave', 'artist');
 
       expect(artists.resolveValue).to.be.eql({ body: 'json' });
+    });
+  });
+
+  describe('searchArtists', () => {
+    it('should call fetch function', () => {
+      const artists = searchArtists('Audioslave');
+      expect(fetchedStub).to.be.calledOnce;
+    });
+
+    it('should call fetch with the correct url', () => {
+      const artists = searchArtists('Audioslave');
+      expect(fetchedStub).to.be.calledWith('https://api.spotify.com/v1/search?q=Audioslave&type=artist');
+    });
+  });
+
+  describe('searchAlbums', () => {
+    it('should call fetch function', () => {
+      const albums = searchAlbums('Audioslave');
+      expect(fetchedStub).to.be.calledOnce;
+    });
+
+    it('should call fetch with the correct url', () => {
+      const albums = searchAlbums('Audioslave');
+      expect(fetchedStub).to.be.calledWith('https://api.spotify.com/v1/search?q=Audioslave&type=album');
+
+      const albums2 = searchAlbums('Greenday');
+      expect(fetchedStub).to.be.calledWith('https://api.spotify.com/v1/search?q=Greenday&type=album');
+    });
+  });
+
+  describe('searchTracks', () => {
+    it('should call fetch function', () => {
+      const tracks = searchTracks('Audioslave');
+      expect(fetchedStub).to.be.calledOnce;
+    });
+
+    it('should call fetch with the correct url', () => {
+      const tracks = searchTracks('Audioslave');
+      expect(fetchedStub).to.be.calledWith('https://api.spotify.com/v1/search?q=Audioslave&type=track');
+    });
+  });
+
+  describe('searchPlaylists', () => {
+    it('should call fetch function', () => {
+      const playlists = searchPlaylists('Audioslave');
+      expect(fetchedStub).to.be.calledOnce;
+    });
+
+    it('should call fetch with the correct url', () => {
+      const playlists = searchPlaylists('Audioslave');
+      expect(fetchedStub).to.be.calledWith('https://api.spotify.com/v1/search?q=Audioslave&type=playlist');
     });
   });
 });
